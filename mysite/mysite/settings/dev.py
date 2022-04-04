45df453a26a69547467f156cf303362cbe08211d
safayetjamil647 <work.jamil4@gmail.com>
2022-04-04 18:05:06 +0600

    first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-kl-dh01cz_q*ws^(6d4+5cn!)$8+y)fe126dl+d*t(^n)=cep6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
