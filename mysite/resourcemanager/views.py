905049cefa1fba06ca5ef988638e48fa104fa519
safayetjamil647 <work.jamil4@gmail.com>
2022-06-04 03:26:41 +0600

    demo

--- mysite/resourcemanager/views.py
+++ mysite/resourcemanager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+import boto3
+import mimetypes
+import os
+from django.http.response import HttpResponse
 
 
 def index(request):
@@ -8,3 +11,62 @@ def index(request):
 
 def ec2_index(request):
     return render(request, 'resourcemanager/ec2_index.html')
+
+
+def download_file(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'ec2-keypair.pem'
+    # Define the full file path
+    filepath = BASE_DIR + '/filedownload/Files/' + filename
+    # Open the file for reading content
+    path = open(filepath, 'r')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
+
+
+def launch_instances(self):
+    # create a new EC2 instance
+    ec2 = boto3.resource('ec2', region_name="us-east-1")
+    instances = ec2.create_instances(
+        ImageId='ami-0b0ea68c435eb488d',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',
+        KeyName='ec2-keypair'
+    )
+    return launch_instances()
+
+
+def get_running_instances(self):
+    ec2_client = boto3.client("ec2", region_name="us-east-1")
+    reservations = ec2_client.describe_instances(Filters=[
+        {
+            "Name": "instance-state-name",
+            "Values": ["running"],
+        }
+    ]).get("Reservations")
+
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            instance_type = instance["InstanceType"]
+            public_ip = instance["PublicIpAddress"]
+            private_ip = instance["PrivateIpAddress"]
+            print(f"{instance_id}, {instance_type}, {public_ip}, {private_ip}")
+    return get_running_instances(self)
+
+
+def linux_index(request):
+    return render(request, 'resourcemanager/linux_index.html')
+
+
+def aws_playground(request):
+    return render(request, 'resourcemanager/aws_playground.html')
--- mysite/resourcemanager/wagtail_hooks.py
+++ mysite/resourcemanager/wagtail_hooks.py
@@ -2,7 +2,7 @@ from django.urls import path, reverse
 from wagtail.core import hooks
 from wagtail.admin.menu import MenuItem, SubmenuMenuItem
 from wagtail.contrib.modeladmin.menus import SubMenu
-from .views import index, ec2_index
+from .views import index, ec2_index, download_file, launch_instances,get_running_instances, linux_index,aws_playground
 
 
 @hooks.register('register_admin_urls')
@@ -10,14 +10,20 @@ def register_calendar_url():
     return [
         path('resources/', index, name='resource'),
         path('resources/ec2/', ec2_index, name='ec2'),
+        path('resources/ec2/keypair', download_file, name='key'),
+        path('resources/ec2/launch/', launch_instances, name='launcher'),
+        path('resources/ec2/all_instances/', get_running_instances, name='allinstances'),
+        path('resources/aws_ground/', aws_playground, name='playground'),
+        path('resources/linux/', linux_index, name='linux'),
     ]
 
 
 @hooks.register('register_admin_menu_item')
 def register_calendar_menu_item():
     menu_items = [
-        MenuItem('More Resources', reverse('resource'), icon_name='date'),
-        MenuItem('EC2', reverse('ec2'), icon_name='date'),
+        MenuItem('AWS Resource', reverse('ec2'), icon_name='date'),
+        MenuItem('AWS Playground', reverse('playground'), icon_name='date'),
+        MenuItem('Linux Terminal', reverse('linux'), icon_name='date'),
     ]
     return SubmenuMenuItem('Resource Manager', SubMenu(menu_items), classnames='icon icon-date')
 
